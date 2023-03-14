4b38859178531c37d7b28bf16ace184567f8e307
benbegins <62623777+benbegins@users.noreply.github.com>
2023-03-14 10:36:32 +0100

    page load

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,17 @@ import swiper from "./components/swiper"
 import animation from "./components/animation"
 
 // Init
+const isInit = false
+
 const init = () => {
+	if (isInit) return
 	swiper()
 	transition()
 	animation()
+	isInit = true
 }
 window.addEventListener("pageshow", init)
+window.addEventListener("load", init)
 
 // Vue app
 Vue.createApp(App).mount("#page")
