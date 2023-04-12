cd22b5de2b06c1aff7e079d5f2d8e33d71cd5a77
benbegins <62623777+benbegins@users.noreply.github.com>
2023-04-12 22:45:12 +0200

    transition fix

--- src/js/main.js
+++ src/js/main.js
@@ -6,19 +6,18 @@ import transition from "./components/transition"
 import swiper from "./components/swiper"
 import animation from "./components/animation"
 
-// Init
-let isInit = false
-
 const init = () => {
-	if (isInit) return
-	isInit = true
 	swiper()
 	transition()
 	animation()
 }
-// window.addEventListener("DOMContentLoaded", init)
-window.addEventListener("pageshow", init)
-window.addEventListener("load", init)
 
 // Vue app
 Vue.createApp(App).mount("#page")
+
+// window.addEventListener("DOMContentLoaded", init)
+window.addEventListener("pageshow", init)
+// window.addEventListener("load", init)
+// window.addEventListener("popstate", init)
+
+// init()
