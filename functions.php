693e68be242f53264b01a04a7f7731bd55311bfa
benbegins <62623777+benbegins@users.noreply.github.com>
2023-09-28 13:00:58 +0200

    remove fade anim

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function wearthlab_theme_register_assets(){
         'style', 
         get_template_directory_uri() . '/dist/main.css',
         array(),
-        '1.0'
+        '1.1'
     );
 
     // JS
@@ -52,7 +52,7 @@ function wearthlab_theme_register_assets(){
         'app', 
         get_template_directory_uri() . '/dist/main.js', 
         array(),
-        '1.0',
+        '1.1',
         true
     );
 
